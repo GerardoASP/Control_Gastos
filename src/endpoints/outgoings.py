3a4ad754d8960a9645100997c5c30a20d16d56f3
GerardoASP <gerardo.sanchezp@autonoma.edu.co>
2023-05-11 21:29:02 -0500

    Modyfing models and endpoints with necesary aspects for BD

--- src/endpoints/outgoings.py
+++ src/endpoints/outgoings.py
@@ -1,63 +1,81 @@
 from flask import Blueprint, request
 from http import HTTPStatus
+import sqlalchemy.exc
+import werkzeug
+from src.database import db
+from datetime import datetime
+from src.models.outgoing import Outgoing,outgoing_schema,outgoings_schema
 
 outgoings = Blueprint("outgoings",__name__,url_prefix="/api/v1/outgoings")
 
-# Data for example purposes
-outgoing_data = [
-    {"id_outgoing": 1, "outgoing_concept": "Papitas", "outgoing_date": "2023-05-07", "outgoing_value": 2000},
-    {"id_outgoing": 2, "outgoing_concept": "Gomitas", "outgoing_date": "2023-05-07", "outgoing_value": 2000},
-    {"id_outgoing": 3, "outgoing_concept": "Frunas", "outgoing_date": "2023-05-07", "outgoing_value": 2000},
-    {"id_outgoing": 4, "outgoing_concept": "Juguito", "outgoing_date": "2023-05-07", "outgoing_value": 2000},
-    {"id_outgoing": 5, "outgoing_concept": "Galletas", "outgoing_date": "2023-05-07", "outgoing_value": 2000},
-];
+
 
 @outgoings.get("/")
 def read_all():
-    return {"data": outgoing_data}, HTTPStatus.OK
+    outgoings = Outgoing.query.order_by(Outgoing.outgoing_value).all()
+    return {"data": outgoings_schema.dump(outgoings)}, HTTPStatus.OK
 
 @outgoings.get("/<int:id>")
 def read_one(id):
-    for outgoing in outgoing_data:
-        if outgoing['id_outgoing'] == id:
-            return {"data": outgoing}, HTTPStatus.OK
-
-    return {"error": "Resource not found"}, HTTPStatus.NOT_FOUND
+    outgoing = Outgoing.query.filter_by(id=id).first()
+    if(not outgoing):
+        return {"error": "Resource not found"}, HTTPStatus.NOT_FOUND
+    return {"data": outgoing_schema.dump(outgoing)}, HTTPStatus.OK
 
 @outgoings.post("/")
 def create():
-    post_data = request.get_json()
-
-    outgoing = {
-        "id_outgoing": len(outgoing_data) + 1,
-        "outgoing_concept": post_data.get('outgoing_concept', 'No outgoing Concept'),
-        "outgoing_date": post_data.get('outgoing_date',None),
-        "outgoing_value": post_data.get('outgoing_value',0)
-    }
-
-    outgoing_data.append(outgoing)
-
-    return {"data": outgoing}, HTTPStatus.CREATED
+    post_data = None
+    try:
+        post_data = request.get_json()
+    except werkzeug.exceptions.BadRequest as e:
+        return {"error": "Post body JSON data not found","message": str(e)}, HTTPStatus.BAD_REQUEST
+    outgoing_date_request = request.get_json().get("outgoing_date", None)
+    outgoing_datee = datetime.strptime(outgoing_date_request, '%Y-%m-%d').date()
+    # Outgoing.id is auto increment!
+    outgoing = Outgoing(income_concept = request.get_json().get("Outgoing_concept", None),
+        outgoing_value = request.get_json().get("outgoing_value", None),
+        income_date = outgoing_datee,
+        user_id = request.get_json().get("user_id", None))
+    try:
+        db.session.add(outgoing)
+        db.session.commit()
+    except sqlalchemy.exc.IntegrityError as e:
+        return {"error": "Invalid resource values","message": str(e)}, HTTPStatus.BAD_REQUEST
+    return {"data": outgoing_schema.dump(outgoing)}, HTTPStatus.CREATED
 
 @outgoings.put('/<int:id>')
 @outgoings.patch('/<int:id>')
 def update(id):
-    post_data = request.get_json()
-    for i in range(len(outgoing_data)):
-        if outgoing_data[i]['id_outgoing'] == id:
-            outgoing_data[i] = {
-                "id_outgoing": len(outgoing_data) + 1,
-                "outgoing_concept": post_data.get('outgoing_concept'),
-                "outgoing_date": post_data.get('outgoing_date'),
-                "outgoing_value": post_data.get('outgoing_value')
-            }
-            return {"data": outgoing_data[i]}, HTTPStatus.OK
-    return {"error": "Resource not found"}, HTTPStatus.NOT_FOUND
+    post_data = None
+    try:
+        post_data = request.get_json()
+    except werkzeug.exceptions.BadRequest as e:
+        return {"error": "Post body JSON data not found","message": str(e)}, HTTPStatus.BAD_REQUEST
+    outgoing = Outgoing.query.filter_by(id=id).first()
+    if(not outgoing):
+        return {"error": "Resource not found"}, HTTPStatus.NOT_FOUND
+    outgoing_date_request = request.get_json().get("outgoing_date", None)
+    outgoing_datee = datetime.strptime(outgoing_date_request, '%Y-%m-%d').date()
+    
+    outgoing.outgoing_concept = request.get_json().get('outgoing_concept', outgoing.outgoing_concept)
+    outgoing.outgoing_value = request.get_json().get('outgoing_value', outgoing.outgoing_value)
+    outgoing.outgoing_date = outgoing_datee
+    outgoing.user_id = request.get_json().get('user_id', outgoing.user_id) 
+    
+    try:
+        db.session.commit()
+    except sqlalchemy.exc.IntegrityError as e:
+        return {"error": "Invalid resource values","message": str(e)}, HTTPStatus.BAD_REQUEST
+    return {"data": outgoing_schema.dump(outgoing)}, HTTPStatus.OK
 
 @outgoings.delete("/<int:id>")
 def delete(id):
-    for i in range(len(outgoing_data)):
-        if outgoing_data[i]['id_outgoing'] == id:
-            del outgoing_data[i]
-            return {"data": ""}, HTTPStatus.NO_CONTENT
-    return {"error": "Resource not found"}, HTTPStatus.NOT_FOUND
+    outgoing = Outgoing.query.filter_by(id=id).first() 
+    if(not outgoing):
+        return {"error": "Resource not found"}, HTTPStatus.NOT_FOUND
+    try:
+        db.session.delete(outgoing)
+        db.session.commit()
+    except sqlalchemy.exc.IntegrityError as e:
+        return {"error": "Resource could not be deleted","message": str(e)}, HTTPStatus.BAD_REQUEST
+    return {"data": ""}, HTTPStatus.NO_CONTENT
